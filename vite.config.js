10009d11a5946abd3a88f426a4923709e1f4bed5
Moch Virgiawan Caesar Ridollohi <109588365+mvirgiawancr@users.noreply.github.com>
2024-12-09 19:39:30 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,13 +8,4 @@ export default defineConfig({
             refresh: true,
         }),
     ],
-    build: {
-        outDir: 'public/build',
-        manifest: true,
-        rollupOptions: {
-            output: {
-                manualChunks: undefined,
-            },
-        },
-    },
 });
