9cddba94c69685ad08f3839854c8381ca1c4aa8d
Moch Virgiawan Caesar Ridollohi <109588365+mvirgiawancr@users.noreply.github.com>
2024-12-09 18:54:37 +0700

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -18,5 +18,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if(config('app.env') !== 'local') {
+            URL::forceScheme('https');
+        }
     }
 }
