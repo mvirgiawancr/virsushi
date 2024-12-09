033f03a4276208b074df1777b4a0c1590e5587f5
Moch Virgiawan Caesar Ridollohi <109588365+mvirgiawancr@users.noreply.github.com>
2024-12-09 18:34:32 +0700

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,17 +1,2 @@
 <?php
-
-// Ganti path ke autoload.php
-require __DIR__ . '/../vendor/autoload.php';
-
-// Ganti path ke bootstrap/app.php
-$app = require_once __DIR__ . '/../bootstrap/app.php';
-
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-);
-
-$response->send();
-
-$kernel->terminate($request, $response);
+require __DIR__ . '/../public/index.php';
